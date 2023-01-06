25c68ddef32e1d76e7a6b8d2df21a21f56eae870
Nino <ninochoux@gmail.com>
2023-01-06 08:14:41 +0100

    BDD a jour

--- SQL/coachbasket.sql
+++ SQL/coachbasket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 28 déc. 2022 à 15:34
+-- Généré le : ven. 06 jan. 2023 à 07:12
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -24,6 +24,51 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `adversaire`
+--
+
+DROP TABLE IF EXISTS `adversaire`;
+CREATE TABLE IF NOT EXISTS `adversaire` (
+  `IdAdversaire` int(11) NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(50) DEFAULT NULL,
+  `LienLogo` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`IdAdversaire`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `adversaire`
+--
+
+INSERT INTO `adversaire` (`IdAdversaire`, `Nom`, `LienLogo`) VALUES
+(1, 'Toulouse BC', NULL),
+(2, 'Chateauroux BC', NULL),
+(3, 'Tournefeuille BC', NULL),
+(4, 'Castres BC', NULL),
+(5, 'Albi BC', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `club`
+--
+
+DROP TABLE IF EXISTS `club`;
+CREATE TABLE IF NOT EXISTS `club` (
+  `Nom` varchar(50) NOT NULL,
+  `Logo` blob,
+  PRIMARY KEY (`Nom`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `club`
+--
+
+INSERT INTO `club` (`Nom`, `Logo`) VALUES
+('Rangeuil BC', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `joueur`
 --
 
@@ -38,24 +83,42 @@ CREATE TABLE IF NOT EXISTS `joueur` (
   `Poids` int(3) NOT NULL,
   `PostePref` varchar(50) NOT NULL,
   `Commentaire` text NOT NULL,
-  `idStatus` int(11) NOT NULL,
+  `idStatut` int(11) DEFAULT NULL,
   PRIMARY KEY (`NumLicence`),
-  KEY `FK_Joueur_status` (`idStatus`)
+  KEY `FK_Joueur_status` (`idStatut`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `joueur`
 --
 
-INSERT INTO `joueur` (`NumLicence`, `Prenom`, `Nom`, `DateNaissance`, `Photo`, `Taille`, `Poids`, `PostePref`, `Commentaire`, `idStatus`) VALUES
-('1', 'Kylian', 'Mbappé', '1998-12-20', '', 178, 73, 'Meneur', '', 1),
-('2', 'Junior', 'Neymar', '1992-02-05', '', 175, 68, 'Interieur', '', 2),
-('3', 'Antoine', 'Dupont', '1996-10-05', '', 174, 96, 'Aile', '', 1),
-('4', 'Guillaume', 'Marchand', '1998-06-05', '', 186, 98, 'Aile', '', 1),
-('5', 'Alexandre', 'Mole', '1995-10-10', '', 186, 73, 'Aile', '', 1),
-('6', 'Durand', 'Raphael', '2000-07-12', '', 183, 80, 'Intérieur', '', 1),
-('12', 'Nino', 'Rigal', '2003-10-12', '', 180, 65, 'Meneur', '', 1),
-('7', 'Nicolas', 'Rousseau', '2002-07-19', '', 175, 70, 'Meneur', '', 1);
+INSERT INTO `joueur` (`NumLicence`, `Prenom`, `Nom`, `DateNaissance`, `Photo`, `Taille`, `Poids`, `PostePref`, `Commentaire`, `idStatut`) VALUES
+('NR343536', 'Nino', 'Rigal', '2003-10-12', '', 179, 74, 'Ailier droit', '', 1),
+('AC950236', 'Alexandre', 'Calmet', '2003-03-09', '', 181, 72, 'Allié gauche', '', 1),
+('RD847425', 'Raphael', 'Durand', '2000-08-24', '', 184, 91, 'Interieur droit', '', 1),
+('CS485696', 'Chloe', 'Sechi', '2003-08-14', '', 170, 60, 'Meneur', '', 1),
+('EC587410', 'Elena', 'Chelle', '2003-04-02', '', 175, 60, 'Allié droit', '', 1),
+('AM693256', 'Alexandre', 'Mole', '1998-10-29', '', 185, 85, 'Interieur gauche', '', 1),
+('NL685320', 'Nelson', 'Lima', '2002-07-09', '', 173, 78, 'Meneur', '', 1),
+('YA563201', 'Yanis', 'Afrite', '2003-05-16', '', 187, 90, 'Interieur droit', '', 1),
+('TN745896', 'Theo', 'Nunes Varela ', '2001-06-26', '', 178, 76, 'Allié droit', '', 1),
+('NG569820', 'Nina', 'Girard', '2001-02-21', '', 168, 62, 'Meneur', '', 1),
+('GD620357', 'Gaia', 'Ducournau', '2003-08-21', '', 164, 58, 'Interieur gauche', '', 1),
+('GM849630', 'Guillaume', 'Pomies', '2000-01-01', '', 200, 93, 'Allié gauche', '', 1),
+('FE445638', 'Fantin', 'Elalouf', '2002-07-09', '', 182, 73, 'Allié droit', '', 4),
+('BL541039', 'Benoit', 'Leclerc', '2004-11-14', '', 193, 84, 'Interieur gauche', '', 2),
+('YT002289', 'Young Way', 'Tsan', '2002-03-19', '', 168, 65, 'Meneur', '', 4),
+('MC960236', 'Mickael', 'Calmet', '2003-09-03', '', 181, 73, 'Allié droit', '', 4),
+('TW447787', 'Theo', 'Wazidrag', '2002-12-03', '', 173, 92, 'Meneur', '', 2),
+('BV663599', 'Bruno', 'Van Den Berg', '2001-09-03', '', 201, 76, 'Allié Gauche', '', 3),
+('NR444562', 'Nicolas', 'Rousseau', '1998-02-25', '', 185, 86, 'Interieur droit', '', 2),
+('RB111200', 'Remi', 'Barberi', '2002-09-03', '', 163, 92, 'Allié droit', '', 3),
+('LC889632', 'Louis', 'Cazals', '2000-04-27', '', 186, 78, 'Interieur droit', '', 3),
+('MI036985', 'Makima', 'Inomiya', '2003-08-14', '', 170, 60, 'Meneur', '', 1),
+('DA554422', 'Denji', 'Arashi', '2003-03-09', '', 181, 72, 'Allié gauche', '', 2),
+('JA503377', 'Jules', 'Alves', '2000-09-22', '', 172, 73, 'Interieur droit', '', 1),
+('SB887306', 'Sasha', 'Birindelli', '2005-04-19', '', 161, 61, 'Allié gauche', '', 4),
+('FG110258', 'Farouk', 'Gonzales', '2001-01-22', '', 165, 86, 'Interieur gauche', '', 1);
 
 -- --------------------------------------------------------
 
@@ -68,7 +131,9 @@ CREATE TABLE IF NOT EXISTS `participer` (
   `Titulaire` tinyint(1) NOT NULL,
   `Notation` char(1) NOT NULL,
   `NumLicence` char(8) NOT NULL,
-  `IdRencontre` int(11) NOT NULL
+  `IdRencontre` int(11) NOT NULL,
+  KEY `FK_Participer` (`NumLicence`),
+  KEY `FK_Participer_Rencontre` (`IdRencontre`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -76,13 +141,35 @@ CREATE TABLE IF NOT EXISTS `participer` (
 --
 
 INSERT INTO `participer` (`Titulaire`, `Notation`, `NumLicence`, `IdRencontre`) VALUES
-(1, '', '1', 1),
-(1, '', '2', 1),
-(1, '', '3', 1),
-(1, '', '4', 1),
-(1, '', '5', 1),
-(0, '', '6', 1),
-(0, '', '6', 1);
+(1, '', 'NG569820', 1),
+(1, '', 'NG569820', 6),
+(1, '', 'RD847425', 1),
+(1, '', 'RD847425', 3),
+(1, '', 'AC950236', 1),
+(1, '', 'AC950236', 10),
+(1, '', 'NR343536', 1),
+(0, '', 'NR343536', 5),
+(1, '', 'CS485696', 1),
+(1, '', 'CS485696', 10),
+(1, '', 'NL685320', 3),
+(1, '', 'NL685320', 6),
+(1, '', 'EC587410', 2),
+(0, '', 'EC587410', 3),
+(1, '', 'YA563201', 3),
+(1, '', 'YA563201', 4),
+(0, '', 'YA563201', 1),
+(1, '', 'TN745896', 4),
+(1, '', 'TN745896', 6),
+(1, '', 'GD620357', 3),
+(1, '', 'GD620357', 2),
+(0, '', 'GD620357', 6),
+(1, '', 'GM849630', 2),
+(1, '', 'GM849630', 3),
+(1, '', 'MI036985', 4),
+(1, '', 'MI036985', 2),
+(0, '', 'MI036985', 7),
+(1, '', 'JA503377', 7),
+(1, '', 'JA503377', 10);
 
 -- --------------------------------------------------------
 
@@ -92,46 +179,56 @@ INSERT INTO `participer` (`Titulaire`, `Notation`, `NumLicence`, `IdRencontre`) 
 
 DROP TABLE IF EXISTS `rencontre`;
 CREATE TABLE IF NOT EXISTS `rencontre` (
-  `IdRencontre` int(11) NOT NULL,
-  `NomEAdverse` varchar(50) NOT NULL,
-  `LieuRencontre` varchar(50) NOT NULL,
-  `Domicile` tinyint(1) NOT NULL,
+  `IDRencontre` int(11) NOT NULL AUTO_INCREMENT,
+  `LieuRencontre` varchar(50) DEFAULT NULL,
+  `Domicile` tinyint(1) DEFAULT NULL,
   `DateRencontre` date NOT NULL,
-  `HeureRencontre` time NOT NULL,
-  `ScoreEquipe` int(3) NOT NULL,
-  `ScoreAdverse` int(3) NOT NULL,
-  PRIMARY KEY (`IdRencontre`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `HeureRencontre` time DEFAULT NULL,
+  `ScoreEquipe` smallint(6) DEFAULT NULL,
+  `ScoreAdverse` smallint(6) DEFAULT NULL,
+  `IdAdversaire` int(11) NOT NULL,
+  PRIMARY KEY (`IDRencontre`),
+  KEY `IdAdversaire` (`IdAdversaire`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `rencontre`
 --
 
-INSERT INTO `rencontre` (`IdRencontre`, `NomEAdverse`, `LieuRencontre`, `Domicile`, `DateRencontre`, `HeureRencontre`, `ScoreEquipe`, `ScoreAdverse`) VALUES
-(1, 'TournefeuilleBC', 'Tournefeuille', 0, '2022-10-12', '08:00:00', 66, 48),
-(2, 'Chicago Bulls', 'Chicago', 0, '2022-03-09', '21:00:00', 12, 98);
+INSERT INTO `rencontre` (`IDRencontre`, `LieuRencontre`, `Domicile`, `DateRencontre`, `HeureRencontre`, `ScoreEquipe`, `ScoreAdverse`, `IdAdversaire`) VALUES
+(1, 'Toulouse', 0, '2021-01-25', '15:00:00', 84, 79, 1),
+(2, 'Toulouse', 0, '2021-01-23', '15:00:00', 63, 91, 1),
+(3, 'Chateauroux', 0, '2021-02-16', '16:00:00', 21, 75, 2),
+(4, 'Tournefeuille', 0, '2021-03-08', '14:45:00', 101, 35, 3),
+(5, 'Toulouse', 1, '2021-03-23', '14:00:00', 85, 83, 5),
+(6, 'Toulouse', 1, '2021-04-16', '15:00:00', 79, 79, 4),
+(7, 'Castres', 0, '2021-04-25', '16:35:00', 45, 90, 4),
+(8, 'Toulouse', 1, '2021-05-10', '16:20:00', 60, 103, 3),
+(9, 'Toulouse', 1, '2021-05-17', '10:00:00', 96, 66, 2),
+(10, 'Albi', 0, '2021-06-01', '20:00:00', 52, 58, 5);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `status`
+-- Structure de la table `statut`
 --
 
-DROP TABLE IF EXISTS `status`;
-CREATE TABLE IF NOT EXISTS `status` (
-  `idStatus` int(11) NOT NULL,
+DROP TABLE IF EXISTS `statut`;
+CREATE TABLE IF NOT EXISTS `statut` (
+  `idStatut` int(11) NOT NULL,
   `Libelle` varchar(50) NOT NULL,
-  PRIMARY KEY (`idStatus`)
+  PRIMARY KEY (`idStatut`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `status`
+-- Déchargement des données de la table `statut`
 --
 
-INSERT INTO `status` (`idStatus`, `Libelle`) VALUES
-(2, 'Blessé(e)'),
-(3, 'En vacances'),
-(1, 'Disponible');
+INSERT INTO `statut` (`idStatut`, `Libelle`) VALUES
+(1, 'Actif'),
+(3, 'Suspendu'),
+(2, 'Blesse'),
+(4, 'Absent');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
